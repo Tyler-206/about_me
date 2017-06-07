86f315b241b58b0c22997b5f08c0c8e5519e4714
Tyler W <tyler.walser@yahoo.com>
2017-06-07 15:29:45 -0700

    finished question 6 in js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var userPoints = 0;
+var total = 7;
+
 var user = prompt ('What is your name?');
 console.log ('User\'s name is ' + user);
 
@@ -10,42 +13,82 @@ console.log ('Question 1: Does Tyler ride the bus to class? ' + user + '\'s answ
 
 if (q1 === 'yes' || q1 === 'y') {
   alert ('Correct! Great job ' + user + ', next question');
+  userPoints++;
 } else {
   alert ('Wrong, tyler rides the bus becuase parking is too expensive! and theres crazy traffic to boot! Next question.');
 };
+
+console.log (user + '\'s Points: ' + userPoints + ' out of ' + total);
 
 var q2 = prompt ('Is Tyler from Seattle? Answer yes or no', 'type yes or no').toLowerCase();
 console.log ('Question 2: Is Tyler from Seattle? ' + user + '\'s answer: ' + q2);
 
 if (q2 === 'yes' || q2 === 'y') {
   alert ('Correct! Great job ' + user + ', next question.');
+  userPoints++;
 } else {
   alert ('Wrong, Tyler is in fact from Seattle, next question.');
 };
+
+console.log (user + '\'s Points: ' + userPoints + ' out of ' + total);
 
 var q3 = prompt ('Is Tyler\'s favorite sports team the Seahawks? Answer yes or no', 'type yes or no').toLowerCase();
 console.log ('Question 3: Is Tyler\'s favorite sports team the Seahawks? ' + user + '\'s answer: ' + q3);
 
 if (q3 === 'no' || q3 === 'n') {
   alert ('Correct! Great job ' + user + ',the Seahawks are Tyler\'s second favorite team. Tyler\'s favorite team is the Sounders, next question.');
+  userPoints++;
 } else {
   alert ('Wrong, The Seahawks are Tyler\'s second favorite team. Tyler\'s favorite team is the Sounders, next question.');
 };
+
+console.log (user + '\'s Points: ' + userPoints + ' out of ' + total);
 
 var q4 = prompt ('Does Tyler have a dog named Woolly Mamoth? Answer yes or no', 'type yes or no' ).toLowerCase();
 console.log ('Question 4: Does Tyler have a dog named Woolly Mamoth? ' + user + '\'s answer: ' + q4);
 
 if (q4 === 'yes' || q4 === 'y') {
   alert ('Correct! Great job ' + user + ', next question.');
+  userPoints++;
 } else {
   alert ('Wrong, Tyler\'s dog is infact named Woolly Mammoth, next question.');
 };
 
-var q5 = prompt ('Final question: Does Tyler have a cat named "cat"? Answer yes or no', 'type yes or no').toLowerCase();
+console.log (user + '\'s Points: ' + userPoints + ' out of ' + total);
+
+var q5 = prompt ('Does Tyler have a cat named "cat"? Answer yes or no', 'type yes or no').toLowerCase();
 console.log ('Question 5: Does Tyler have a cat named "cat"? ' + user + '\'s answer: ' + q5);
 
 if (q5 === 'no' || q5 === 'n') {
   alert ('Correct! Tyler\'s cat is named Dinosaur. Great job ' + user + ', thanks for taking a stab at these get to know me questions!');
+  userPoints++;
 } else {
   alert ('Wrong, Tyler\'s cat is named Dinosaur, thanks for trying to answer these get to know me questions!');
 };
+
+console.log (user + '\'s Points: ' + userPoints + ' out of ' + total);
+
+
+var tries = 1;
+
+while (tries <= 4 && a6 !== 9){
+  var q6 = prompt ('What is my favorite number?');
+  console.log ('Question 6: What is my favorite number ' + user + '\'s answer: ' + q6);
+
+  var a6 = parseInt(q6);
+  console.log (a6);
+
+  if (a6 < 9){
+    alert ('too low, try again! You have used ' + tries + ' out of 4 attempts');
+  } else if (a6 === 9){
+    alert ('Good Job!');
+  } else {
+    alert ('too high, try again! You have used ' + tries + ' out of 4 attempts');
+  }
+  tries++;
+}
+
+alert('You get a point wether you got it or not!');
+userPoints++;
+
+console.log (user + '\'s Points: ' + userPoints + ' out of ' + total);
