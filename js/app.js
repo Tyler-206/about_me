115783c3c16316c372b66905a02e9ac6dc0868b1
Tyler W <tyler.walser@yahoo.com>
2017-06-11 18:48:46 -0700

    added seperate page for guessing game and updated css

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ console.log ('Question 1: Does Tyler ride the bus to class? ' + user + '\'s answ
 if (q1 === 'yes' || q1 === 'y') {
   alert ('Correct! Great job ' + user + ', next question');
   userPoints++;
+
 } else {
   alert ('Wrong, Tyler rides the bus becuase parking is too expensive! and theres crazy traffic to boot! Next question.');
 };
@@ -96,6 +97,7 @@ tries = 1;
 var states = ['missouri', 'new york', 'florida', 'california'];
 
 var q7 = prompt ('What states have I lived in other than Washington?').toLowerCase();
+
 while (tries <= 6 && states.indexOf(q7) === -1 ){
 
   console.log ('Question 7: What states have I lived in? Try #: ' + tries + ' ' + user + '\'s answer: ' + q7);
@@ -108,6 +110,7 @@ if (states.indexOf(q7) !== -1) {
   alert ('Good Job!');
   userPoints++;
 }
+
 console.log (user + '\'s Points: ' + userPoints + ' out of ' + total);
 
 alert('I have lived in Washington, Missouri, New York, Florida and California! ' + user + ', you answered ' + userPoints + ' out of ' + total + ' questions! Thanks for playing!!');
